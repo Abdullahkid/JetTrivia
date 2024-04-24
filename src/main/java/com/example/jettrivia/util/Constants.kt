75b06f50498cbc2b99dcb154def3567fa49de90b
Abdullahkid <109651913+Abdullahkid@users.noreply.github.com>
2024-04-24 17:48:33 +0530

    Add files via upload

---
+++ src/main/java/com/example/jettrivia/util/Constants.kt
@@ -0,0 +1,12 @@
+package com.example.jettrivia.util
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+
+object Constants {
+    //https://raw.githubusercontent.com/itmmckernan/triviaJSON/master/world.json
+    const val BASE_URL = "https://raw.githubusercontent.com/itmmckernan/triviaJSON/master/"
+
+}
